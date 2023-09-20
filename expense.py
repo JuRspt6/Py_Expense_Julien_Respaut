f8e0b77438fa210fef988c88235bd71f9f81cea7
JuRspt <julien@respaut.fr>
2023-09-20 20:23:40 +0200

    First push with 5 features

--- expense.py
+++ expense.py
@@ -1,4 +1,13 @@
 from PyInquirer import prompt
+import csv
+
+def get_spenders(*args):
+    spenders = []
+    with open('users.csv', 'r') as users_file:
+        reader = csv.reader(users_file)
+        for row in reader:
+            spenders.append({"name": row[0]})
+    return spenders
 
 expense_questions = [
     {
@@ -12,19 +21,25 @@ expense_questions = [
         "message":"New Expense - Label: ",
     },
     {
-        "type":"input",
+        "type":"list",
         "name":"spender",
         "message":"New Expense - Spender: ",
+        "choices": get_spenders,
+    },
+    {
+        "type":"checkbox",
+        "name":"involved_persons",
+        "message":"New Expense - Involved persons: ",
+        "choices": get_spenders,
     },
 
 ]
 
-
-
 def new_expense(*args):
     infos = prompt(expense_questions)
+    csv_writer = csv.writer(open("expense_report.csv","a"))
+    csv_writer.writerow([infos['amount'], infos['label'], infos['spender'], infos['involved_persons']])
+
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
     print("Expense Added !")
     return True
-
-
--- user.py
+++ user.py
@@ -1,8 +1,18 @@
 from PyInquirer import prompt
-user_questions = [
+import csv
 
+user_questions = [
+    {
+        "type":"input",
+        "name":"name",
+        "message":"New User - Name: ",
+    }
 ]
 
 def add_user():
     # This function should create a new user, asking for its name
+    infos = prompt(user_questions)
+    csv_writer = csv.writer(open("users.csv","a"))
+    csv_writer.writerow([infos['name']])
+
     return
